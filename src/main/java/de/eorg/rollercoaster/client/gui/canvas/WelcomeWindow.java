ac63fd379f064029585e1d4781e22e40e56f1c53
strikecom <strikecom@gmx.net>
2013-09-12 09:26:50 +0200

    Pom auf offizielles Google-Plugin umgestellt -> Hotswapping

--- src/main/java/de/eorg/rollercoaster/client/gui/canvas/WelcomeWindow.java
+++ src/main/java/de/eorg/rollercoaster/client/gui/canvas/WelcomeWindow.java
@@ -57,7 +57,7 @@ public class WelcomeWindow extends Window {
 		welcomeInfo.setBackgroundColor("#ffffff");
 
 		Label welcomeLabel = new Label(
-				"<h1 style=\"font-size: 40pt\">Welcome!</h1><p style=\"font-size: 20pt\"><span style=\"font-weight: bolder;\">The Crawler</span> <span style=\"font-style: italic; font-weight: bolder;\">n.</span> is an enhanced, continuously growing database of Amazon Machine Images (AMI). By permanently crawling public AMIs from the Amazon EC2 service, the Crawler collects information about the contents of AMIs, i.e., software libraries and versions. The large database supports DevOps, Cloud Developers and Cloud users with insights to compare and understand AMIs better. A search engine allows to find AMIs with a configuration that meets standards and requirements.</p><p style=\"font-size: 20pt\">Feel free to ride the Crawler and <a href=\"http://myownthemepark.com/prices-table/\">contact us for a premium account</a>. Simply sign in with a social user account (Facebook, Twitter, Google+).</p>");
+				"<h1 style=\"font-size: 40pt\">Welcome!</h1><p style=\"font-size: 20pt\"><span style=\"font-weight: bolder;\">The Rollercoaster</span> <span style=\"font-style: italic; font-weight: bolder;\"> </span> is an enhanced... TEST</p><p style=\"font-size: 20pt\">Feel free to ride the Rollercoaster and <a href=\"http://myownthemepark.com/prices-table/\">contact us for a premium account</a>. Simply sign in with a social user account (Facebook, Twitter, Google+).</p>");
 		welcomeLabel.setWidth(600);
 		welcomeLabel.setStyleName("welcome");
 
